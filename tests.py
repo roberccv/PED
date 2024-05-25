0dd949b088d3aed72f24c4d17eadbe14a180b2a3
roberccv <roberto.cinosvega@usp.ceu.es>
2024-05-25 19:52:15 +0000

    Test 3

--- tests.py
+++ tests.py
@@ -8,5 +8,10 @@ class TestRespuesta(unittest.TestCase):
         hora = datetime.datetime.now().strftime("%H:%M:%S")
         self.assertEqual(contestacion_servidor, hora)
 
+    def test_respuesta_Fecha(self):
+        solucion = Respuesta()
+        contestacion_servidor = solucion.respuesta_del_servidor(b"Fecha")
+        fecha = datetime.datetime.now().strftime("%Y-%m-%d")
+        self.assertEqual(contestacion_servidor, fecha)
 if __name__ == '__main__':
     unittest.main()
