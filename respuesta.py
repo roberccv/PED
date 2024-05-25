fdacde4eafe08141654e43eff0686fc8f0a242b0
roberccv <roberto.cinosvega@usp.ceu.es>
2024-05-25 19:50:32 +0000

    Test 1 OK

--- respuesta.py
+++ respuesta.py
@@ -1,3 +1,6 @@
+import datetime
 class Respuesta:
     def respuesta_del_servidor(self, mensaje):
-        pass
+        if mensaje == b"Hora":
+            return datetime.datetime.now().strftime("%H:%M:%S")
+        else: pass
