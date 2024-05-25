b76206bab7e1da62dd4ac91847d9fe1ec30c694e
roberccv <roberto.cinosvega@usp.ceu.es>
2024-05-25 20:05:18 +0000

    test 4 OK

--- respuesta.py
+++ respuesta.py
@@ -1,7 +1,9 @@
 import datetime
 class Respuesta:
     def respuesta_del_servidor(self, mensaje):
-        if mensaje == b"Hora":
+        if not isinstance(mensaje, bytes):
+            raise TypeError("El mensaje debe ser de tipo bytes")
+        elif mensaje == b"Hora":
             return datetime.datetime.now().strftime("%H:%M:%S")
         elif mensaje == b"Fecha":
             return datetime.datetime.now().strftime("%Y-%m-%d")
