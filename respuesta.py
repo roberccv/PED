779ec7242706c3f2d4ccb46ef721b8ade4f8e1c4
roberccv <roberto.cinosvega@usp.ceu.es>
2024-05-25 19:53:01 +0000

    Test 2 OK

--- respuesta.py
+++ respuesta.py
@@ -3,4 +3,6 @@ class Respuesta:
     def respuesta_del_servidor(self, mensaje):
         if mensaje == b"Hora":
             return datetime.datetime.now().strftime("%H:%M:%S")
+        elif mensaje == b"Fecha":
+            return datetime.datetime.now().strftime("%Y-%m-%d")
         else: pass
