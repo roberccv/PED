5bbcb05f13b0d12d0da51c80c734e6eb6074b658
roberccv <roberto.cinosvega@usp.ceu.es>
2024-05-26 10:04:11 +0000

    Test 2 OK

--- Prueba_2/figura.py
+++ Prueba_2/figura.py
@@ -2,4 +2,6 @@ class Figura:
 
     def area(self, X_max, X_min, Y_max, Y_min):
         return ((X_max - X_min) * (Y_max - Y_min))
+    def perimetro(self, X_max, X_min, Y_max, Y_min):
+        return ((X_max - X_min) * 2 + (Y_max - Y_min) * 2)
         
