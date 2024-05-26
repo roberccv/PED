e08eab0bd1977bbfcbe52b2939425850998808bf
roberccv <roberto.cinosvega@usp.ceu.es>
2024-05-26 10:27:51 +0000

    servidor

---
+++ Prueba_2/servidor_sT.py
@@ -0,0 +1,45 @@
+import socket, sys, os
+from figura import Figura
+
+puerto = int(sys.argv[1])
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+
+    s.bind(('localhost', puerto ))
+
+    s.listen(2)
+
+    while True:
+        ns, _ = s.accept()
+
+        atender_cliente(ns)
+
+    
+def atender_cliente(ns):
+    pid = os.fork()
+    if pid == 0:
+        while True:
+            calculo = ns.recv(1024)
+            ns.send("Siguiente")
+            X_max = ns.recv(1024)
+            ns.send("Siguiente")
+            X_min = ns.recv(1024)
+            ns.send("Siguiente")
+            Y_max = ns.recv(1024)
+            ns.send("Siguiente")
+            Y_min = ns.recv(1024)
+
+            fig = Figura()
+            if calculo == "area":
+                respuesta = fig.area(X_max, X_min, Y_max, Y_min)
+            elif calculo == "perimetro":
+                respuesta = fig.perimetro(X_max, X_min, Y_max, Y_min)
+            elif calculo == "punto_medio":
+                respuesta = fig.punto_medio(X_max, X_min, Y_max, Y_min)
+            else:
+                respuesta = "Error"
+            ns.send(respuesta)
+            ns.close()
+            break
+if __name__ == '__main__':
+    main()
