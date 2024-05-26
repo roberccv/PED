1db5611752a4e0010b85a646c411d1d0341db21e
roberccv <roberto.cinosvega@usp.ceu.es>
2024-05-26 10:01:15 +0000

    Test 1

---
+++ Prueba_2/test.py
@@ -0,0 +1,12 @@
+from figura import Figura
+import unittest
+
+#X_max , X_min , Y_max , Y_min
+class TestFigura(unittest.TestCase):
+    def test_area(self):
+        fig = Figura()
+        area = fig.area(10, 0, 3,2)
+        self.assertEqual(area, ((10-0)*(3-2)))
+
+if __name__ == '__main__':
+    unittest.main()
